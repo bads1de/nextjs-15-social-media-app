9c74abe5e541b4c5e43802e097ec0b1a279463da
s3xmaxman <badside3919@gmail.com>
2024-09-12 09:09:13 +0900

    create 2 files, copy 1 file and update 1 file

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,12 @@
-import signupImage from "@/assets/signup.png";
+import loginImage from "@/assets/login.png";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import SignUpForm from "./SignUpForm";
+// import LoginForm from "./LoginForm";
 
 // TODO: 後でデザインを修正する
 export const metadata: Metadata = {
-  title: "サインアップ",
+  title: "ログイン",
 };
 
 export default function Page() {
@@ -14,22 +14,17 @@ export default function Page() {
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
-          <div className="space-y-1 text-center">
-            <h1 className="text-3xl font-bold">サインアップ</h1>
-            <p className="text-muted-foreground">
-              あなたも友達を見つけられる場所です。
-            </p>
-          </div>
+          <h1 className="text-center text-3xl font-bold">ログイン</h1>
           <div className="space-y-5">
-            <SignUpForm />
-            <Link href="/login" className="block text-center hover:underline">
-              すでにアカウントをお持ちですか？
+            LoginForm
+            <Link href="/signup" className="block text-center hover:underline">
+              アカウントをお持ちの方はこちら
             </Link>
           </div>
         </div>
         <Image
-          src={signupImage}
-          alt="サインアップ画像"
+          src={loginImage}
+          alt="ログイン画像"
           className="hidden w-1/2 object-cover md:block"
         />
       </div>
