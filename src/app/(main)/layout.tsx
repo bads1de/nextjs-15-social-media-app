e0c4b9e604b383d8a76e466f191154c1ae3951aa
s3xmaxman <badside3919@gmail.com>
2024-09-12 12:52:16 +0900

    update layout.tsx

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
+import Navbar from "./Navbar";
 
 export default async function Layout({
   children,
@@ -16,7 +17,7 @@ export default async function Layout({
   return (
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
-        Navbar
+        <Navbar />
         <div className="mx-auto max-w-7xl p-5">{children}</div>
       </div>
     </SessionProvider>
