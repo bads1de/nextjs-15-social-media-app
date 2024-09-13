ed7df26fcfa968ccf1f11580fe3ad44257236201
s3xmaxman <badside3919@gmail.com>
2024-09-14 01:37:45 +0900

    update FollowButton.tsx

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -22,16 +22,30 @@ export default function FollowButton({
   const queryClient = useQueryClient();
   const queryKey: QueryKey = ["follower-info", userId];
 
+  /**
+   * フォロー/フォロー解除を行うMutation
+   *
+   * @remarks
+   * useMutationフックを使用して、フォロー/フォロー解除の非同期処理を管理します。
+   *
+   * mutationFn: フォロー/フォロー解除を行うAPIリクエストを送信する関数
+   * onMutate: APIリクエストが送信される前に実行される関数。ここでは、楽観的なUI更新を行い、キャッシュを更新します。
+   * onError: APIリクエストが失敗した場合に実行される関数。ここでは、エラーメッセージを表示し、キャッシュを以前の状態に戻します。
+   */
   const { mutate } = useMutation({
     mutationFn: () =>
       data.isFollowedByUser
         ? kyInstance.delete(`/api/users/${userId}/followers`)
         : kyInstance.post(`/api/users/${userId}/followers`),
     onMutate: async () => {
+      // 現在のクエリをキャンセル
       await queryClient.cancelQueries({ queryKey });
 
+      // 以前のキャッシュデータを取得
       const previousState = queryClient.getQueryData<FollowerInfo>(queryKey);
 
+      // 楽観的なUI更新
+      // フォロー/フォロー解除の状態を反転させ、フォロワー数を更新
       queryClient.setQueryData<FollowerInfo>(queryKey, () => ({
         followers:
           (previousState?.followers || 0) +
@@ -39,11 +53,14 @@ export default function FollowButton({
         isFollowedByUser: !previousState?.isFollowedByUser,
       }));
 
+      // エラー発生時にキャッシュを戻すために、以前の状態を返す
       return { previousState };
     },
     onError(error, variables, context) {
+      // エラー発生時、キャッシュを以前の状態に戻す
       queryClient.setQueryData<FollowerInfo>(queryKey, context?.previousState);
       console.error(error);
+      // エラートーストを表示
       toast({
         title: "エラー",
         description: "フォローの変更に失敗しました",
