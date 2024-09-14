d2489467bc211197d04e3809f4702c9b2a962b0c
s3xmaxman <badside3919@gmail.com>
2024-09-14 11:50:03 +0900

    update UserPosts.tsx and page.tsx

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -11,6 +11,7 @@ import { formatDate } from "date-fns";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { cache } from "react";
+import UserPosts from "./UserPosts";
 
 interface PageProps {
   params: {
@@ -69,12 +70,12 @@ export default async function Page({ params: { username } }: PageProps) {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <UserProfile user={user} loggedInUserId={loggedInUser.id} />
-        <div className="rounded-2xl bg-card p-5 shadow-sm">
-          <h2 className="text-center text-2xl font-bold">
-            {user.displayName}&apos;s posts
-          </h2>
-        </div>
-        UserPosts
+        {/* <div className="rounded-2xl bg-card p-5 shadow-sm">
+          {/* <h2 className="text-center text-2xl font-bold">
+            {user.displayName}&apos;s Posts
+          </h2> */}
+        {/* </div> */}
+        <UserPosts userId={user.id} />
       </div>
       <TrendsSidebar />
     </main>
