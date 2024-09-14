1b75c4b15daa3890083217d0a641395874f3c115
s3xmaxman <badside3919@gmail.com>
2024-09-14 10:24:26 +0900

    update page.tsx and tabs.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,25 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 import ForYouFeed from "./ForYouFeed";
-
 export default async function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">おすすめ</TabsTrigger>
+            <TabsTrigger value="following">フォロー中</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
