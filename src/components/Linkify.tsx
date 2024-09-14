aa2cde7b6e5a48f58c30e5acf67ea870aa7e4201
s3xmaxman <badside3919@gmail.com>
2024-09-14 16:32:20 +0900

    update 3 files and delete 2 files

---
+++ src/components/Linkify.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import { LinkIt, LinkItUrl } from "react-linkify-it";
+import UserLinkWithTooltip from "./UserLinkWithTooltip";
+// import UserLinkWithTooltip from "./UserLinkWithTooltip";
+
+interface LinkifyProps {
+  children: React.ReactNode;
+}
+
+export default function Linkify({ children }: LinkifyProps) {
+  return (
+    <LinkifyUsername>
+      <LinkifyHashtag>
+        <LinkifyUrl>{children}</LinkifyUrl>
+      </LinkifyHashtag>
+    </LinkifyUsername>
+  );
+}
+
+function LinkifyUrl({ children }: LinkifyProps) {
+  return (
+    <LinkItUrl className="text-primary hover:underline">{children}</LinkItUrl>
+  );
+}
+
+function LinkifyUsername({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/(@[a-zA-Z0-9_-]+)/}
+      component={(match, key) => {
+        return (
+          <UserLinkWithTooltip key={key} username={match.slice(1)}>
+            {match}
+          </UserLinkWithTooltip>
+        );
+      }}
+    >
+      {children}
+    </LinkIt>
+  );
+}
+
+function LinkifyHashtag({ children }: LinkifyProps) {
+  return (
+    <LinkIt
+      regex={/(#[\p{L}\p{N}_-]+)/u}
+      component={(match, key) => {
+        return (
+          <Link
+            href={`/hashtag/${encodeURIComponent(match.slice(1))}`}
+            className="text-primary hover:underline"
+            key={key}
+          >
+            {match}
+          </Link>
+        );
+      }}
+    >
+      {children}
+    </LinkIt>
+  );
+}
---
+++ src/components/UserLinkWithTooltip.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import kyInstance from "@/lib/ky";
+import { UserData } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+import { HTTPError } from "ky";
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+import UserTooltip from "./UserTooltip";
+
+interface UserLinkWithTooltipProps extends PropsWithChildren {
+  username: string;
+}
+
+export default function UserLinkWithTooltip({
+  username,
+  children,
+}: UserLinkWithTooltipProps) {
+  const { data } = useQuery({
+    queryKey: ["user-data", username],
+    // クエリ関数で、ユーザーデータを取得するAPIを呼び出します。
+    queryFn: () =>
+      kyInstance.get(`/api/users/username/${username}`).json<UserData>(),
+    // エラーが発生した場合のリトライ処理を定義します。
+    retry(failureCount, error) {
+      // HTTPErrorが発生し、ステータスコードが404の場合はリトライしません。
+      if (error instanceof HTTPError && error.response.status === 404) {
+        return false;
+      }
+      // それ以外の場合は、3回までリトライします。
+      return failureCount < 3;
+    },
+    // データの有効期限を無限に設定します。
+    staleTime: Infinity,
+  });
+
+  if (!data) {
+    return (
+      <Link href={`/user/${username}`} className="text-primary hover:underline">
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <UserTooltip user={data}>
+      <Link
+        href={`/user/${data.username}`}
+        className="text-primary hover:underline"
+      >
+        {children}
+      </Link>
+    </UserTooltip>
+  );
+}
