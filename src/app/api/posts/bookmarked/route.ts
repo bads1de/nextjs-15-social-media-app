1d45832889bde58be562676e2737b319eef0e04c
s3xmaxman <badside3919@gmail.com>
2024-09-16 21:39:15 +0900

    feat: create route.ts and route.ts

---
+++ src/app/api/posts/bookmarked/route.ts
@@ -0,0 +1,4 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getPostDataInclude, PostsPage } from "@/lib/types";
+import { NextRequest } from "next/server";
