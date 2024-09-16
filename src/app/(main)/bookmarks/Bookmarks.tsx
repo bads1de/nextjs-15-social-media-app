d3f066a5160cf76b7819c142c0a9a62ca0caf61c
s3xmaxman <badside3919@gmail.com>
2024-09-16 22:03:23 +0900

    feat: create Bookmarks.tsx and page.tsx

---
+++ src/app/(main)/bookmarks/Bookmarks.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import kyInstance from "@/lib/ky";
+import { PostsPage } from "@/lib/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+export default function Bookmarks() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "bookmarks"],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/bookmarked",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (status === "pending") {
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center">
+        <p className="text-center text-lg">まだブックマークされていません</p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">エラーが発生しました</p>
+    );
+  }
+
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
+  );
+}
